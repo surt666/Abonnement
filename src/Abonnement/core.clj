757bbea3f741aa8e15983b853a490dc2baf930aa
Steen Larsen <steen666@gmail.com>
2011-09-08 12:03:26 +0200

    first commit

---
+++ src/Abonnement/core.clj
@@ -0,0 +1,1 @@
+(ns Abonnement.core)
---
+++ test/Abonnement/test/core.clj
@@ -0,0 +1,6 @@
+(ns Abonnement.test.core
+  (:use [Abonnement.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

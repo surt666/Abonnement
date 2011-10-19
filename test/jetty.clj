3abdb85eb1ff6c3c87d2550e2d3edbb8905dc9b1
Steen Larsen <steen666@gmail.com>
2011-10-19 09:43:02 +0200

    added jetty

---
+++ test/jetty.clj
@@ -0,0 +1,17 @@
+(ns jetty
+  (:use ring.adapter.jetty)
+  (:use Abonnement.routes)  
+  (:import (org.mortbay.xml XmlConfiguration)
+	   (org.mortbay.jetty.webapp WebAppContext)))
+
+(defn init-server [server]
+  (try 
+    (let [config (XmlConfiguration. (slurp "test/jetty.xml"))]   
+      (. config configure server))
+    (catch Exception e
+      (prn "Unable to load jetty configuration")
+      (. e printStackTrace))))
+
+
+(defonce server
+  (run-jetty #'app {:port 8080 :configurator init-server :join? false}))
